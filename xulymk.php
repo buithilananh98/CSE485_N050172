07e2022db9ea6c4bb8888c73029b38064d41435c
buithilananh98 <44974712+buithilananh98@users.noreply.github.com>
2018-12-19 11:22:13 +0700

    from đăng ký

---
+++ xulymk.php
@@ -0,0 +1,27 @@
+<html>
+<head>
+	<title>Trang đăng nhập</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" href="dangnhap.css">
+</head>
+<body>
+	<form method="POST" action="dangnhap.php">
+	<fieldset>
+	    <h4>Đăng nhập</h4>
+	    	<table>
+	    		<tr>
+	    			<td>Username</td>
+	    			<td><input type="text" name="username" ></td>
+	    		</tr>
+	    		<tr>
+	    			<td>Password</td>
+	    			<td><input type="password" name="password" ></td>
+	    		</tr>
+	    		<tr>
+	    			<td colspan="2" align="center"> <input type="submit" value="Đăng nhập"></td>
+	    		</tr>
+	    	</table>
+  </fieldset>
+  </form>
+</body>
+</html>
